74e2034da35b68428208048d6e18a790e92edafb
Arjun Khetia <arjunkhetia@gmail.com>
2020-01-12 19:40:42 +0530

    Angular Universal Project (Server Side Rendering)

---
+++ src/app/app.server.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
+import { UniversalInterceptor } from './universal-interceptor';
+import { ServerModule } from '@angular/platform-server';
+import { AppModule } from './app.module';
+import { AppComponent } from './app.component';
+
+@NgModule({
+  imports: [
+    AppModule,
+    ServerModule,
+  ],
+  providers: [{
+    provide: HTTP_INTERCEPTORS,
+    useClass: UniversalInterceptor,
+    multi: true
+  }],
+  bootstrap: [AppComponent],
+})
+export class AppServerModule {}
